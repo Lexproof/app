8279a9113f9c8e40e7909d262a79b4851fa79005
Ryan Waits <ryan.waits@gmail.com>
2023-02-14 12:10:44 -0600

    bring api call back

--- components/claim-search.tsx
+++ components/claim-search.tsx
@@ -10,56 +10,19 @@ import { Button } from '@/ui/button';
 // import { Checkbox } from '@/ui/checkbox';
 import { Input } from '@/ui/input';
 
-// async function getClaims() {
-//   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/claims`);
-//   return await data.json();
-// }
+async function getClaims() {
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/claims`);
+  return await data.json();
+}
 
 export function ClaimSearch({ claims }) {
   const [selectedClaims, setSelectedClaims] = React.useState([]);
-  // const { data, isLoading, isRefetching, isStale, isFetching, isFetched } =
-  //   useQuery({
-  //     queryKey: ['claims'],
-  //     queryFn: getClaims,
-  //     initialData: claims,
-  //   });
-  const data = [
-    {
-      id: 1,
-      name: 'Networth > $1M',
-      icon: 1,
-      tldr: 'A liquid networth of $1M or more needed for eligibility.',
-      price: '60',
-    },
-    {
-      id: 2,
-      name: 'Income > $200k for last 2 years',
-      icon: 2,
-      tldr: 'Income tax return of $200k or more for the last 3 financial years.',
-      price: '60',
-    },
-    {
-      id: 3,
-      name: 'Accredited License Letter',
-      icon: 3,
-      tldr: 'Letter from an accountant, licensed securities broker or registered investment advisor.',
-      price: '60',
-    },
-    {
-      id: 4,
-      name: 'U.S. Citizenship',
-      icon: 4,
-      tldr: 'You are currently a citizen of the U.S.',
-      price: '45',
-    },
-    {
-      id: 5,
-      name: 'Residency',
-      icon: 5,
-      tldr: 'Currently a resident of the U.S. and for the last 2 years.',
-      price: '45',
-    },
-  ];
+  const { data, isLoading, isRefetching, isStale, isFetching, isFetched } =
+    useQuery({
+      queryKey: ['claims'],
+      queryFn: getClaims,
+      initialData: claims,
+    });
   const [filter, setFilter] = React.useState('');
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
