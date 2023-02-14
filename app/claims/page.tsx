3396a7e3424bfa603b58cacb2312f92fd0b4f3da
Ryan Waits <ryan.waits@gmail.com>
2023-02-14 12:06:33 -0600

    remove api call

--- app/claims/page.tsx
+++ app/claims/page.tsx
@@ -3,13 +3,51 @@ import { useQuery } from '@tanstack/react-query';
 
 import { ClaimSearch } from '@/components/claim-search';
 
-async function getClaims() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/claims`);
-  return await data.json();
-}
+// async function getClaims() {
+//   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/claims`);
+//   return await data.json();
+// }
+
+const claims = [
+  {
+    id: 1,
+    name: 'Networth > $1M',
+    icon: 1,
+    tldr: 'A liquid networth of $1M or more needed for eligibility.',
+    price: '60',
+  },
+  {
+    id: 2,
+    name: 'Income > $200k for last 2 years',
+    icon: 2,
+    tldr: 'Income tax return of $200k or more for the last 3 financial years.',
+    price: '60',
+  },
+  {
+    id: 3,
+    name: 'Accredited License Letter',
+    icon: 3,
+    tldr: 'Letter from an accountant, licensed securities broker or registered investment advisor.',
+    price: '60',
+  },
+  {
+    id: 4,
+    name: 'U.S. Citizenship',
+    icon: 4,
+    tldr: 'You are currently a citizen of the U.S.',
+    price: '45',
+  },
+  {
+    id: 5,
+    name: 'Residency',
+    icon: 5,
+    tldr: 'Currently a resident of the U.S. and for the last 2 years.',
+    price: '45',
+  },
+];
 
 export default async function Claims(props) {
-  const claims = await getClaims();
+  // const claims = await getClaims();
   return (
     <>
       <main className='flex flex-col items-center justify-center'>
