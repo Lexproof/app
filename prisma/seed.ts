229e357dfa51a906d1ba4e1845dcf12bca65201b
Ryan Waits <ryan.waits@gmail.com>
2023-02-24 12:10:50 -0600

    lint updates

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,36 @@
-import prisma from '@/lib/prisma';
+// import prisma from '@/lib/prisma';
 
-async function main() {
-  const kyb = await prisma.package.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: 'KYB',
-      description:
-        'Provides information and documentation to verify the identity and ownership of a business, helping to ensure compliance and prevent fraud.',
-      price: 0,
-      icon: '1',
-      active: true,
-      offers: {
-        create: {
-          name: 'Check out Prisma with Next.js',
-          description: 'https://www.prisma.io/nextjs',
-          price: 0,
-          icon: '1',
-          active: true,
-        },
-      },
-    },
-  });
+// async function main() {
+//   const kyb = await prisma.package.upsert({
+//     where: { id: 1 },
+//     update: {},
+//     create: {
+//       name: 'KYB',
+//       description:
+//         'Provides information and documentation to verify the identity and ownership of a business, helping to ensure compliance and prevent fraud.',
+//       price: 0,
+//       icon: '1',
+//       active: true,
+//       offers: {
+//         create: {
+//           name: 'Check out Prisma with Next.js',
+//           description: 'https://www.prisma.io/nextjs',
+//           price: 0,
+//           icon: '1',
+//           active: true,
+//         },
+//       },
+//     },
+//   });
 
-  console.log({ kyb });
-}
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+//   console.log({ kyb });
+// }
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
